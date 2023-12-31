e7aeb8120786868cd3fd1a897065cce38afdf4f7
Khôi Nguyễn <originalkhoi151@gmail.com>
2023-12-31 11:28:24 +0700

    added interface for creating song

--- controllers/admin/song.controller.ts
+++ controllers/admin/song.controller.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from 'express';
 
 import Song from '../../models/song.model';
+import Topic from '../../models/topic.model';
+import Singer from '../../models/singer.model';
 
 // [GET] /admin/songs
 export const index = async (req: Request, res: Response) => {
@@ -12,6 +14,32 @@ export const index = async (req: Request, res: Response) => {
     res.render('admin/pages/songs/index.pug', {
       pageTitle: 'Song List',
       songs: songs
+    })
+
+  } catch (error) {
+    console.log('Error occurred in [GET] /admin/songs:', error);
+    res.json({
+      code: 400,
+      message: "Not existed"
+    })
+  }
+}
+
+// [GET] /admin/songs/create
+export const create = async (req: Request, res: Response) => {
+  try {
+    const topics = await Topic.find({
+      deleted: false
+    }).select('title');
+
+    const singers = await Singer.find({
+      deleted: false
+    }).select('fullName');
+
+    res.render('admin/pages/songs/create', {
+      pageTitle: 'Create Song',
+      topics: topics,
+      singers: singers
     })
 
   } catch (error) {
--- models/song.model.ts
+++ models/song.model.ts
@@ -1,4 +1,7 @@
 import mongoose from "mongoose";
+import slug from 'mongoose-slug-updater'
+
+mongoose.plugin(slug);
 
 const songSchema = new mongoose.Schema(
   {
